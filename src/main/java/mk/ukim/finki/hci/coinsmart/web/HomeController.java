483d3f539a326138ba40ee5f5f8cf2b2270234fa
Zagorka <jovanovazagorka@outlook.com>
2022-07-02 16:03:25 +0200

    add home page

---
+++ src/main/java/mk/ukim/finki/hci/coinsmart/web/HomeController.java
@@ -0,0 +1,16 @@
+package mk.ukim.finki.hci.coinsmart.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/home")
+public class HomeController {
+
+    @GetMapping
+    public String getHomePage(Model model){
+        return "home";
+    }
+}
