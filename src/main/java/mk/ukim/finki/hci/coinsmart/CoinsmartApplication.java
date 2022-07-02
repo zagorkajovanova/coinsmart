e9e5bedf1e749c9cb7f43a2fe26f1118a8b47a1b
Zagorka <jovanovazagorka@outlook.com>
2022-07-02 12:16:10 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/hci/coinsmart/CoinsmartApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.hci.coinsmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinsmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinsmartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/hci/coinsmart/CoinsmartApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.hci.coinsmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinsmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
