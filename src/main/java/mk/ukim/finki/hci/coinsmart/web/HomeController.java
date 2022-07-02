04f3ebd89aaaea5a3aa5d45b9d1030db0a79a7c4
Zagorka <jovanovazagorka@outlook.com>
2022-07-02 16:43:10 +0200

    update HomeController and modify home page

--- src/main/java/mk/ukim/finki/hci/coinsmart/web/HomeController.java
+++ src/main/java/mk/ukim/finki/hci/coinsmart/web/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/home")
+@RequestMapping(value = {"/","/home"})
 public class HomeController {
 
     @GetMapping
