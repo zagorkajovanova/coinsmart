c282faffc65fc8ce8f25812653baa31fed02d18b
Zagorka <jovanovazagorka@outlook.com>
2022-07-04 16:18:53 +0200

    add all course pages; update MainController

--- src/main/java/mk/ukim/finki/hci/coinsmart/web/MainController.java
+++ src/main/java/mk/ukim/finki/hci/coinsmart/web/MainController.java
@@ -3,7 +3,9 @@ package mk.ukim.finki.hci.coinsmart.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping
@@ -28,5 +30,52 @@ public class MainController {
         model.addAttribute("bodyContent", "what-is-cryptocurrency");
         model.addAttribute("pageTitle", "What is Cryptocurrency?");
         return "master-template";
+    }
+
+    @GetMapping("/learn/what-is-blockchain")
+    public String getCourseTwoPage(Model model){
+        model.addAttribute("bodyContent", "what-is-blockchain");
+        model.addAttribute("pageTitle", "What is Blockchain?");
+        return "master-template";
+    }
+
+    @GetMapping("/learn/what-is-ethereum")
+    public String getCourseThreePage(Model model){
+        model.addAttribute("bodyContent", "what-is-ethereum");
+        model.addAttribute("pageTitle", "What is Ethereum?");
+        return "master-template";
+    }
+
+    @GetMapping("/learn/what-is-bitcoin")
+    public String getCourseFourPage(Model model){
+        model.addAttribute("bodyContent", "what-is-bitcoin");
+        model.addAttribute("pageTitle", "What is Bitcoin?");
+        return "master-template";
+    }
+
+    @GetMapping("/learn/bitcoin-scarcity-trust-in-money")
+    public String getCourseFivePage(Model model){
+        model.addAttribute("bodyContent", "bitcoin-scarcity");
+        model.addAttribute("pageTitle", "Bitcoin, scarcity & trust in money");
+        return "master-template";
+    }
+
+    @GetMapping("/learn/building-on-top-of-bitcoin")
+    public String getCourseSixPage(Model model){
+        model.addAttribute("bodyContent", "building-on-top");
+        model.addAttribute("pageTitle", "Building on top of Bitcoin");
+        return "master-template";
+    }
+
+    @GetMapping("learn/completed-course")
+    public String getCompletedCoursePage(Model model){
+        model.addAttribute("bodyContent", "completed-course");
+        model.addAttribute("pageTitle", "Course completed");
+        return "master-template";
+    }
+
+    @PostMapping("/learn/finished")
+    public String markCourse(@RequestParam String course){
+        return "redirect:/learn/completed-course";
     }
 }
